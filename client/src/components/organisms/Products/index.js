09f7c5de2f13b655195965fc0e28a61f5c6c4895
zachcb <zach.c.barnes@gmail.com>
2019-12-18 21:44:56 -0700

    Add Products

--- client/src/components/organisms/Products/index.js
+++ client/src/components/organisms/Products/index.js
@@ -3,18 +3,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import ProductCard from "../../molecules/ProductCard";
-import styles from "./styles.css";
+import styles from "./styles.module.css";
 
-const Products = ({ products }) => (
+const Products = ({ products, handleAddCartItem }) => (
   <div className={styles.wrapper}>
     {_.map(products, product => (
-      <ProductCard {...product} />
+      <ProductCard key={product.title} handleAddCartItem={handleAddCartItem} {...product} />
     ))}
   </div>
 );
 
 Products.propTypes = {
-  products: PropTypes.object
+  products: PropTypes.array,
+  handleAddCartItem: PropTypes.func
 };
 
 export default Products;
