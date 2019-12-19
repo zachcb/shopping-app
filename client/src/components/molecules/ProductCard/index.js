7745841edebb9421ce5fccd26c119d139cdeffa7
zachcb <zach.c.barnes@gmail.com>
2019-12-18 21:44:37 -0700

    Add markup and styles for ProductCard

--- client/src/components/molecules/ProductCard/index.js
+++ client/src/components/molecules/ProductCard/index.js
@@ -1,6 +1,26 @@
 import React from "react";
-import styles from "./styles.css";
+import PropTypes from "prop-types";
 
-const ProductCard = () => <button>Add To Cart</button>;
+import styles from "./styles.module.css";
+
+const ProductCard = ({ id, title, description, imageUrl, handleAddCartItem }) => (
+  <div className={styles.wrapper} onClick={() => handleAddCartItem(id)}>
+    <div className={styles["img-container"]}>
+      <img src={imageUrl}></img>
+    </div>
+    <div className={styles.content}>
+      <header>{title}</header>
+      <p>{description}</p>
+    </div>
+  </div>
+);
+
+ProductCard.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  imageUrl: PropTypes.string,
+  handleAddCartItem: PropTypes.func
+};
 
 export default ProductCard;
